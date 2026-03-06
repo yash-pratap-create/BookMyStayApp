94d851764603f5ddbce0f502a5e6b81798b52e93
yash-pratap-create <robinhoodyash15@gmail.com>
2026-03-06 15:40:43 +0530

    Refactor BookmystayApp class and clean comments
    Removed unnecessary comments and updated class name.

--- app/src/BookmystayApp.java
+++ app/src/BookmystayApp.java
@@ -1,28 +1,3 @@
-/**
- * ===============================================================
- * MAIN CLASS - UseCase1HotelBookingApp
- * ===============================================================
- *
- * Use Case 1: Application Entry & Welcome Message
- *
- * Description:
- * This class represents the entry point of the
- * Hotel Booking Management System.
- *
- * At this stage, the application:
- * - Starts execution from the main() method
- * - Displays a welcome message to the user
- * - Confirms that the system has started successfully
- *
- * No business logic, data structures, or user input
- * is implemented in this use case.
- *
- * The goal is to establish a clear and predictable
- * application startup point.
- *
- * @author Developer
- * @version 1.0
- */
 
 public class HotelBookingApp {
 
@@ -42,7 +17,6 @@ public class HotelBookingApp {
         // Display application name and version
         System.out.println("Book My Stay App v1.0");
 
-        // Confirm successful initialization
-        System.out.println("System initialized successfully.");
+        
     }
 }
